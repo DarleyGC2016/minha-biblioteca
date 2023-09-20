3b2604e7feb9544cdd3893a4f014fa7e57526802
DarleyGC2016 <garciacardoso.darley@gmail.com>
2023-09-20 10:44:05 -0300

    task 1 - listagem, edição e excluir

--- src/components/BooksList/index.jsx
+++ src/components/BooksList/index.jsx
@@ -2,22 +2,22 @@ import { useCallback } from 'react'
 import { Link } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import Api from "../../services/api"
+import './estilo.css'
 
 function BookList() {
     const { data, mutate } = useFetch('livros');
+
     const bookUpdate = useCallback(async (id) => {
-        Api.put(`livros/${id}`, {
-            nome: 'Aprendendo a programar 8'
+        Api.delete(`livros/${id}`);
+        data?.map(livro => {
+            if (livro.id === id){
+                mutate({...livro, nome: 'Aprendendo a programar 15'})
+                return livro;
+            }
+            return livro;
         })
-        if (!data) {  
-            data.map(livro =>{
-                if (livro.id === id){
-                    mutate({...livro, nome: 'Aprendendo a programar 8'})
-                }
-                mutate(false)
-            })
-        }
-        
+
+        mutate(false)            
         
     },[ data, mutate ])
     
@@ -25,21 +25,25 @@ function BookList() {
         return <p>Carregando...</p>
     }
 
-    return (
-        <ul>
-            {
-                data.map(livro => (
-                    <li key={livro.id}>
-                        <Link to={`/livros/${livro.id}`}>
-                            {livro.nome}
-                        </Link>
-                        <button type="button" onClick={() => bookUpdate(livro.id)}>
-                            Editar
-                        </button>
-                    </li>
-                ))
-            }
-        </ul>
+    return (  
+        <div className='lista'>
+                <ul className='listagem'>
+                    {
+                        data.map(livro => (
+                            <li key={livro.id} className='item'>
+                                {livro.nome}
+                                <Link to={`/livros/${livro.id}`} className='btn'>
+                                    Editar   
+                                </Link>
+                                <button type="button" className='btn-excluir' onClick={() => bookUpdate(livro.id)}>
+                                    Excluir
+                                </button>
+                            </li>
+                        ))
+                    }
+                </ul>
+        </div>      
+        
     )
 }
 
