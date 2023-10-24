28a8c940865732dc4514cc518c05b90cbfc1a459
DarleyGC2016 <garciacardoso.darley@gmail.com>
2023-10-24 18:28:55 -0300

    task 6 - final

--- src/pages/__tests __/BookDetail.test.jsx
+++ src/pages/__tests __/BookDetail.test.jsx
@@ -2,6 +2,15 @@ import { render, screen } from '@testing-library/react'
 import BookDetail from '../BookDetail/index'
 import "@testing-library/jest-dom"
 
+const mockNavigate = jest.fn()
+const mockParams = jest.fn()
+
+jest.mock('react-router-dom', () => {
+    return  {    
+                useParams: () => mockParams,
+                useNavigate: () => mockNavigate
+            }
+})
 
 const renderComponent = () => {
     render(<BookDetail />)
